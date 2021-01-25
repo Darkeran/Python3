1b632b630e15fa20cfdd67e77b849fad15b1201f
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 14:44:57 -0300

    Update Produto.py

--- Produto.py
+++ Produto.py
@@ -1,4 +1,4 @@
-Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato. 
+#Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato. 
 
 produto1 = float(input("Digite o preço em R$ do primeiro produto:", ))
 produto2 = float(input("Digite o preço em R$ do segundo produto:", ))
@@ -6,7 +6,9 @@ produto3 = float(input("Digite o preco em R$ do terceito produto:", ))
 
 if produto1 < produto2 and produto1 < produto3:
     print("O Primeiro produto é o mais em conta dos três produtos")
+    
 elif produto2 < produto1 and produto2 < produto3:
     print("Segundo produto é o mais em conta dos três produtos")
+    
 elif produto3 < produto1 and produto3 < produto2:
     print("O Terceiro produto é o mais em conta dos três produtos")
