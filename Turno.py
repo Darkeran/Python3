b2d98d572491a7f3d9e3ca8b27070dcbf47b3bbe
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 14:41:00 -0300

    Create Turno.py

---
+++ Turno.py
@@ -0,0 +1,11 @@
+Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+turno = str(input("Qual periodo em que voce estuda, (M)-Matutino, (V)-Vespertino ou (N)-Noturno:" ).upper())
+if turno == "M":
+    print("Bom dia")
+elif turno == "N":
+    print("Boa noite")
+elif turno == "V":
+    print("Boa tarde")
+else:
+    print("Valor invalido")
