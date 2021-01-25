4ce369d6cb7cb27e82c984b8c7710ee2def0c8b4
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 14:56:43 -0300

    Create Soma_Numeros.py

---
+++ Soma_Numeros.py
@@ -0,0 +1,8 @@
+''' Faça um Programa que peça dois números e imprima a soma. '''
+
+n1=int(input("digite o primeiro numero: "))
+n2=int(input("digite o segundo numero: "))
+
+soma = n1 + n2
+
+print("A soma entre {} e {} é igual a {}" .format(n1, n2, soma))
