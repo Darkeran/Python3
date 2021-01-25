09c2be9e7a40eb822fbccd3f68c1c7fa035fae65
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 14:47:34 -0300

    Update Sexo.py

--- Sexo.py
+++ Sexo.py
@@ -1,9 +1,12 @@
-Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. 
+'''Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido. '''
 
 sexo = str  (input("Digite (F)-Feminino ou (M)-Masculino: ").upper())
+
 if sexo == "M":
     print("Seu sexo é masculino")
+    
 elif sexo == "F":
     print("Seu sexo é feminino")
+    
 else:
     print("Sexo invalido")
