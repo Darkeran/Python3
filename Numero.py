5b07b24511106eda0b048e889da256bd3bab9e6b
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 14:55:36 -0300

    Create Numero.py

---
+++ Numero.py
@@ -0,0 +1,4 @@
+'''Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. '''
+
+numero = input("Insira um número: ",)
+print("O número que você digitou é", numero)
