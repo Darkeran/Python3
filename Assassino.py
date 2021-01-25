e9191877e8dc397eec526210807d38dc8486ce1f
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 17:51:53 -0300

    Update Assassino.py

--- Assassino.py
+++ Assassino.py
@@ -1,5 +1,5 @@
-Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
-''' "Telefonou para a vítima?"
+''' Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As perguntas são:
+    "Telefonou para a vítima?"
     "Esteve no local do crime?"
     "Mora perto da vítima?"
     "Devia para a vítima?"
