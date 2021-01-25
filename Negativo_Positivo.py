8901349555080844bb55117fe73ae72e8fc8e7f4
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-25 15:23:03 -0300

    Create Negativo_Positivo.py

---
+++ Negativo_Positivo.py
@@ -0,0 +1,9 @@
+''' Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.  '''
+
+numero = int    (input("Digite um numero:", ))
+if numero < 0:
+    print("O numero {} é negativo".format(numero))
+elif numero == 0:
+     print("O numero {} é neutro".format(numero))
+else:
+     print("O numero {} é positivo".format(numero))
