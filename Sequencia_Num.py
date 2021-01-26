0d2e3b20f8f3d2149f48ea9fc5fc92d6c8c7f3d8
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-26 15:52:15 -0300

    Create Sequencia_Num.py

---
+++ Sequencia_Num.py
@@ -0,0 +1,15 @@
+''' Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+Depois modifique o programa para que ele mostre os números um ao lado do outro. '''
+
+
+numero_cres = 0
+while numero_cres < 21:
+    numero_cres = numero_cres + 1
+    print(numero_cres)
+
+lista = []
+numero_seq = 0
+while numero_seq < 21:
+    numero_seq = numero_seq + 1
+    lista.append(numero_seq)
+print(lista)
