f4e77fac672d31cadba2630d0c825474b15203e6
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-26 19:09:13 -0300

    Create Base_Expoente.py

---
+++ Base_Expoente.py
@@ -0,0 +1,8 @@
+''' Faça um programa que peça dois números, base e expoente, calcule e mostre o primeiro número elevado ao 
+segundo número. Não utilize a função de potência da linguagem.                                        '''
+
+Base = int(input("Digite um numero base: ",))
+Expoente = int(input("Digite um número expoente: ", ))
+
+Potencia = Base ** Expoente
+print("O número {} elevado a {} é igual a {}".format(Base, Expoente, Potencia))
