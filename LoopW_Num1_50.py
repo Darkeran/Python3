8997076b331380846ab51dcd3b03ef60716b0460
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:44:01 -0300

    Update LoopW_Num1_50.py

--- LoopW_Num1_50.py
+++ LoopW_Num1_50.py
@@ -1,4 +1,4 @@
-''' Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50. '''
+# Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50.
 
 numero = 0
 while numero < 50:
