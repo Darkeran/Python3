10a5fa3fdf3bee3f720f864707b09951af31c6f2
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:40:47 -0300

    Update Fahrenheit_Celsius.py

--- Fahrenheit_Celsius.py
+++ Fahrenheit_Celsius.py
@@ -1,4 +1,4 @@
-''' Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.  '''
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
 
 
 fahrenhait = float (input("Insira a temperatura em Fahrenhait:" ))
