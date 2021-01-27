c734cb867851835aed27898702235ffc9b77ec07
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:51:33 -0300

    Update Salario.py

--- Salario.py
+++ Salario.py
@@ -1,4 +1,4 @@
-''' Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.  '''
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês. 
 
 
 horas = float(input("Quantas horas você trabalha por mês: " ))
