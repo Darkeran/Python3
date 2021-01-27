42883b45feefeb4cf246624390a251e585de43ec
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:52:06 -0300

    Update Sequencia_Num.py

--- Sequencia_Num.py
+++ Sequencia_Num.py
@@ -1,5 +1,5 @@
-''' Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
-Depois modifique o programa para que ele mostre os números um ao lado do outro. '''
+# Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. 
+# Depois modifique o programa para que ele mostre os números um ao lado do outro.
 
 
 numero_cres = 0
