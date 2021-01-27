4a0cf5b843769fd92ae2ec20233353def0d8d43d
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:48:30 -0300

    Update Metro_Centimetro.py

--- Metro_Centimetro.py
+++ Metro_Centimetro.py
@@ -1,4 +1,4 @@
-''' Faça um Programa que converta metros para centímetros. '''
+# Faça um Programa que converta metros para centímetros.
 
 metros = float(input("Insira quantos metros voce quer converter para centimetros:", ))
 
