5e932ff4b5272418c333afbb97fb10acb2338bbe
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-27 09:43:36 -0300

    Update LoopW_MediaNum5.py

--- LoopW_MediaNum5.py
+++ LoopW_MediaNum5.py
@@ -1,4 +1,4 @@
-''' Faça um programa que leia 5 números e informe a soma e a média dos números. '''
+# Faça um programa que leia 5 números e informe a soma e a média dos números. 
 
 soma_num = 0
 numero = 0
