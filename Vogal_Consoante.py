99112fdf66fee7a9c5b44e2b3d6e05550d71b234
D4RKER4N <75955064+Darkeran@users.noreply.github.com>
2021-01-30 09:27:05 -0300

    Delete Vogal_Consoante.py

--- Vogal_Consoante.py
+++
@@ -1,9 +0,0 @@
-# Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-
-letra = str(input("Digite qualquer letra:" ).upper())
-
-if letra == "A" or letra == "E" or letra == "I" or letra == "O" or letra == "U" :
-    print("A letra {} é uma vogal".format(letra))
-    
-else:
-    print("A letra {} é uma consoante".format(letra))
